d4bfbaa461f05191cad77702916d9f0540a7556d
Tom Houdmont <tomhoudmont@gmail.com>
2013-07-23 17:34:42 +0100

    Form fields created

--- index.php
+++ index.php
@@ -15,30 +15,110 @@
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
 
-        <!-- Optional Modernizr, if it were required in the long run -->
-        <!-- <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
+
+        <!-- Add a little help for old browsers -->
+
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
         <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <div class="container">
+            <h1>Fake Registration Form</h1>
+
+            <hr>
+
+            <div class="alert alert-error" id="error" style="display: none;">
+                <strong>Warning!</strong> Make sure you've entered a name.
+            </div>
+
+            <form id="registration" class="form-horizontal well" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+
+                <div class="control-group">
+                    <p>
+                        Fields with a <span style="color:red;">*</span> are required fields.
+                    </p>
+                </div>
+
+                <div class="control-group">
+                    <label class="control-label" for="inputName">Name <span style="color:red;">*</span></label>
+                    <div class="controls">
+                        <input type="text" id="inputName" placeholder="Name">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="inputDate">Date of Birth</label>
+                    <div class="controls">
+                        <input type="date" id="inputName" placeholder="Date of Birth">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="inputEmail">Email</label>
+                    <div class="controls">
+                        <input type="email" id="inputEmail" placeholder="Email">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="inputPassword">Password</label>
+                    <div class="controls">
+                        <input type="password" id="inputPassword" placeholder="Password">
+                    </div>
+                </div>
+
+                <div class="control-group">
+                    <label class="control-label">
+                        Gender
+                    </label>
+                    <div class="controls">
+                        <label for="inputGenderMale" class="radio inline">
+                            <input type="radio" name="inputGender" id="inputGenderMale" value="Male" checked>
+                            Male
+                        </label>
+                        <label for="inputGenderFemale" class="radio inline">
+                            <input type="radio" name="inputGender" id="inputGenderFemale" value="Female">
+                            Female
+                        </label>
+                    </div>
+                </div>
+
+                 <div class="control-group">
+                    <label class="control-label">
+                        I use HTML5?
+                    </label>
+                    <div class="controls">
+                        <label for="inputHTML5Yes" class="radio inline">
+                            <input type="radio" name="inputHTML5" id="inputHTML5Yes" value="Yes" checked>
+                            Yes
+                        </label>
+                        <label for="inputHTML5No" class="radio inline">
+                            <input type="radio" name="inputHTML5" id="inputHTML5No" value="No">
+                            No
+                        </label>
+                    </div>
+                </div>
+
+                <div class="control-group">
+                    <div class="controls">
+                        <label class="checkbox">
+                            <input type="checkbox" name="inputMarketing"> Add me to the mailing list
+                        </label>
+                    </div>
+                </div>
+
+                <div class="form-actions">
+                    <button type="submit" class="btn">Register</button>
+                </div>
+            </form>
+        </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+
+        <!-- In Production - these would be concatenated together. -->
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
     </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
+// Example of loading a polyfill when no native placeholder functionality is found.
+Modernizr.load([
+    {
+        test: Modernizr.input.placeholder,
+        nope:   [
+                    '../css/placeholder_polyfill.css',
+                    '../js/placeholder_polyfill.jquery.js',
+                ]
+    }
+]);
 
+// Example of initializing the css for a plugin based on available
+// browser functionality.
+if( ! Modernizr.inputtypes.date) {
+
+    Modernizr.load({
+        test: Modernizr.inputtypes.date,
+        nope: ['/js/jquery-ui-datepicker.min.js','../css/ui-lightness/jquery-ui-1.10.3.custom.min.css'],
+        complete: function() {
+            $('input[type="date"]').datepicker();
+        },
+    });
+}
+
+
+// Caching variables.
+var $doc = $(document),
+    $error = $('#error'),
+    $form  = $('#registration');
+
+
+$doc.on('submit', $form, function(event) {
+
+    if( ! $('#inputName').val()) {
+
+        $error.show();
+
+        event.preventDefault();
+        return false;
+    } else {
+        $error.hide();
+    }
+});
